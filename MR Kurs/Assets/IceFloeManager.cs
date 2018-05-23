5674ceb6e9246f5f7f0a0f7312d578cb8e04555f
Sarah <u31586@hs-harz.de>
2018-05-23 18:34:58 +0200

    - added ice floe manager script - added manger object in hierachy

---
+++ MR Kurs/Assets/IceFloeManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IceFloeManager : MonoBehaviour {
+
+
+
+
+    void Start () {
+
+
+        //NavMesh.SamplePosition
+
+
+}
+	
+	void Update () {
+		
+	}
+}
