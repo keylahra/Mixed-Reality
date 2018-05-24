c185d9bd9f9e6e4b8c8d2bd61a1e0a2628c54a6b
Simon Nebl <mrcrimsonman@googlemail.com>
2018-05-24 10:52:04 +0200

    Hitboxscript für Eisschollen

---
+++ MR Kurs/Assets/Scripts/Hotspot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hotspot : MonoBehaviour {
+
+    public delegate void HotspotEntered();
+    public static event HotspotEntered OnEntered;
+
+    public delegate void HotspotExited();
+    public static event HotspotExited OnExited;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered!");
+        if (OnEntered != null)
+        {
+            OnEntered();
+            }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Entered!");
+        if (OnExited != null)
+        {
+            OnExited();
+        }
+    }
+}
